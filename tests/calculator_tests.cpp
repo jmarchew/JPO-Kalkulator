32477e8e302d3885212873b4238b77ed1b061aba
jmarchew <jakubm763@gmail.com>
2025-01-25 20:50:21 +0100

    added more tests

--- tests/calculator_tests.cpp
+++ tests/calculator_tests.cpp
@@ -85,9 +85,110 @@ TEST(CalculatorTest, hexAddTest) {
     EXPECT_DOUBLE_EQ(calc.add(), 15.0);  
 }
 
-
-TEST(CalculatorTest, SetAnswerTest) {
+TEST(CalculatorTest, dec2bin) {
     Calculator calc;
-    calc.setAns(42.0);
-    EXPECT_DOUBLE_EQ(calc.getAns(), 42.0);
+    std::string value = "10";
+    int prevMode = 0;
+    calc.setMode(1);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "1010");
+}
+
+TEST(CalculatorTest, dec2oct) {
+    Calculator calc;
+    std::string value = "10";
+    int prevMode = 0;
+    calc.setMode(2);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "12");
+}
+
+TEST(CalculatorTest, dec2hex) {
+    Calculator calc;
+    std::string value = "10";
+    int prevMode = 0;
+    calc.setMode(3);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "A");
+}
+
+TEST(CalculatorTest, bin2dec) {
+    Calculator calc;
+    std::string value = "1010";
+    int prevMode = 1;
+    calc.setMode(0);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "10");
+}
+
+TEST(CalculatorTest, bin2oct) {
+    Calculator calc;
+    std::string value = "1010";
+    int prevMode = 1;
+    calc.setMode(2);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "12");
+}
+
+TEST(CalculatorTest, bin2hex) {
+    Calculator calc;
+    std::string value = "1010";
+    int prevMode = 1;
+    calc.setMode(3);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "A");
+}
+
+TEST(CalculatorTest, oct2dec) {
+    Calculator calc;
+    std::string value = "12";
+    int prevMode = 2;
+    calc.setMode(0);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "10");
+}
+
+TEST(CalculatorTest, oct2bin) {
+    Calculator calc;
+    std::string value = "12";
+    int prevMode = 2;
+    calc.setMode(1);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "1010");
+}
+
+TEST(CalculatorTest, oct2hex) {
+    Calculator calc;
+    std::string value = "12";
+    int prevMode = 2;
+    calc.setMode(3);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "A");
+}
+
+TEST(CalculatorTest, hex2dec) {
+    Calculator calc;
+    std::string value = "A";
+    int prevMode = 3;
+    calc.setMode(0);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "10");
+}
+
+TEST(CalculatorTest, hex2bin) {
+    Calculator calc;
+    std::string value = "A";
+    int prevMode = 3;
+    calc.setMode(1);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "1010");
+}
+
+TEST(CalculatorTest, hex2oct) {
+    Calculator calc;
+    std::string value = "A";
+    int prevMode = 3;
+    calc.setMode(2);
+    std::string result = calc.convert(value, prevMode);
+    EXPECT_EQ(result, "12");
 }
